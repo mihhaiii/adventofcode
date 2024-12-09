04f3e3710575f30c2f114f827fc6f7bd493e3e50
mihhaiii <mihaichihai03@gmail.com>
2024-12-09 16:33:42 +0200

    day9: changed list->dict but same performance

--- day9.py
+++ day9.py
@@ -1,17 +1,21 @@
 import bisect
+import time
+start_time = time.time()
+start_time_c = time.perf_counter()
+start_time_p = time.process_time()
 with open('day9Input') as f:
     a = [int(x) for x in f.read().strip()]
     b = []
     is_file = True
     file_id = 0
 
-    free = [[] for _ in range(10)]
+    free = [{} for _ in range(10)]
     for i, x in enumerate(a):
         if is_file:
             b += [file_id for _ in range(x)]
             file_id += 1
         else:
-            free[x].append(len(b))
+            free[x][len(b)] = None
             b += [-1 for _ in range(x)]
         is_file = not is_file
 
@@ -38,20 +42,25 @@ with open('day9Input') as f:
             for l in range(a[i], 10):
                 if free[l]:
                     found = True
-                    if free[l][0] < min_idx:
-                        min_idx, min_idx_len = free[l][0], l
+                    curr_idx = next(iter(free[l]))
+                    if curr_idx < min_idx:
+                        min_idx, min_idx_len = curr_idx, l
             if found and min_idx < current_pos:
                 for j in range(min_idx, min_idx + a[i]):
                     c[j] = id
                 for j in range(current_pos, current_pos + a[i]):
                     c[j] = -1
-                free[min_idx_len].pop(0)
+                free[min_idx_len].pop(next(iter(free[min_idx_len])))
                 new_len = min_idx_len - a[i]
                 if new_len:
-                    pos = bisect.bisect(free[new_len], min_idx + a[i])
-                    free[new_len].insert(pos, min_idx + a[i])
+                    #pos = bisect.bisect(free[new_len], min_idx + a[i])
+                    #free[new_len].insert(pos, min_idx + a[i])
+                    free[new_len][min_idx + a[i]] = None
 
     ans = sum([i * x for i, x in enumerate(b) if x != -1])
     ans2 = sum([i * x for i, x in enumerate(c) if x != -1])
     print(ans)
     print(ans2)
+    print(f'time = {(time.time() - start_time):.2f} seconds')
+    print(f'time = {time.perf_counter() - start_time_c:.2f} seconds')
+    print(f'time = {time.process_time() - start_time_p:.2f} seconds')
